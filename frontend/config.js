65f14f08ce9fbb59f3c02b912566e1c124c75181
Taiwo Jegede <112409241+TheJegede@users.noreply.github.com>
2026-02-27 00:13:09 -0500

    Set API_URL for production environment

--- frontend/config.js
+++ frontend/config.js
@@ -4,5 +4,5 @@
  * Leave empty for localhost development (auto-detects http://localhost:8000).
  */
 window.APP_CONFIG = {
-    API_URL: ''
+    API_URL: 'https://gcvvcqzs3j.execute-api.us-east-2.amazonaws.com/prod'
 };
